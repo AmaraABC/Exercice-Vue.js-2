2f90001ceae41a19df858b25235fce60ad0a90d3
AmaraABC <amara-noah.gakou@efrei.net>
2025-02-13 17:29:58 +0100

    LoginVue.vue

--- src/pages/LoginVue.vue
+++ src/pages/LoginVue.vue
@@ -1,16 +1,44 @@
 <template>
     <main class="login-main">
+        <h1>Page de connexion</h1>
         <section>
-            <FormComponent />
-                <FieldComponent/>
-                <ButtonComponent/>
+            <FormComponent :data="{id: fields.id, type: fields.type, name: fields.name, placeholder:
+                fields.placeholder}"/>
         </section>
     </main>
 </template>
 
 <script setup lang="ts">
-import FieldComponent from '../components/FieldComponent.vue';
-import ButtonComponent from '../components/ButtonComponent.vue';
+import { ref, reactive } from 'vue';
+import FormComponent from '../components/FormComponent.vue';
+
+const formData = reactive({
+    fields: [
+        {
+            id: 'mail',
+            type: 'email',
+            name: 'mail',
+            placeholder: 'Adresse e-mail',
+        },
+        {
+            id: 'secret-passwd',
+            type: 'password',
+            name: 'secret-passwd',
+            placeholder: 'Mot de passe',
+            minlength: '8',
+            maxlength: '50'
+        },
+    ],
+    buttons: [
+        {
+            id: 'btn',
+            textContent: 'Valider',
+            class: '',
+            name: 'btn',
+            type: '',
+        },
+    ]
+});
 </script>
 
 <style lang="scss"></style>
